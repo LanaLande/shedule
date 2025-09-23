15ce367f2cc2735379f803dd880ed358f6d3feae
LanaLande <uliasvirko27@gmail.com>
2025-09-23 21:39:34 +0300

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import {VitePWA} from 'vite-plugin-pwa'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions :{
+        enabled : true
+      },
+      manifest: {
+        name : "Lana's Lande shedule",
+        short_name : "Shedule",
+        description : 'Ничего в голову не приходит',
+        theme_color : '#ffffff',
+        display : 'standalone',
+        icons : [
+          {
+            src : 'ico192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src : 'ico512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
